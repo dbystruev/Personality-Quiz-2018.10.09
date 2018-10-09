d93ee1b4941cfa5cfcfd4154aaf47026c17b549e
Denis Bystruev <dbystruev@me.com>
2018-10-09 21:51:57 +0300

    Created user interface

--- Personality Quiz/IntroductionViewController.swift
+++ Personality Quiz/IntroductionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  IntroductionViewController.swift
 //  Personality Quiz
 //
 //  Created by Denis Bystruev on 09/10/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class IntroductionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
